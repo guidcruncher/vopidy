7302d8a00f507ec0ef73f7219427d73feba2b969
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-11 20:08:22 +0000

    personalised

--- packages/server/src/commands/tts-time.ts
+++ packages/server/src/commands/tts-time.ts
@@ -7,7 +7,21 @@ export const execute: JsonRpcCommand = async (message: JsonRpcMessage) => {
   const ttsClient = new tts()
   let res = {}
   let date = Config.localDateString()
-  await ttsClient.speak(message.params["lang"], date)
+  let lang = message.params["lang"]
+  if (config.nightEndHour && config.nightEndHour == date.getHours()) {
+    await ttsClient.speak(lang, `Good Morning, It's ${date}`)
+  } else {
+    if (config.nightStartHour && config.nightStartHour == date.getHours()) {
+      await ttsClient.speak(lang, `It's ${date}, Good night.`)
+    } else {
+      if (date.getHours() == 12) {
+        await ttsClient.speak(lang, `Good afternoon, It's ${date}`)
+      } else {
+        await ttsClient.speak(lang, `It's ${date}`)
+      }
+    }
+  }
+
   return date
 }
 
