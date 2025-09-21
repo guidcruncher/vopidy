6a247b9f8862149fcd41a403bf88ece6d3ea9bf6
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-21 01:56:28 +0000

    resizer

---
+++ packages/client/src/composables/useresizer.ts
@@ -0,0 +1,35 @@
+import { emit } from '@/composables/useeventbus'
+import { useResizeObserver } from '@vueuse/core'
+import { onBeforeMount, onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
+
+export function useResizer(target: string, heightOffset: number = 0) {
+  const el = useTemplateRef(target)
+  const cols = ref(0)
+  const width = ref(0)
+  const height = ref(0)
+
+  function start() {
+    useResizeObserver(el, (entries) => {
+      const entry = entries[0]
+      let { w, h } = entry.contentRect
+      if (h == 0) {
+        h = window.innerHeight
+      }
+
+      cols.value = w
+      width.value = w
+      height.value = h - heightOffset
+      emit('resized', { cols: cols.value, size: { width: width.value, height: height.value } })
+    })
+  }
+
+  onBeforeMount(() => start())
+  onMounted(() => {
+    width.value = window.innerWidth
+    cols.value = window.innerWidth
+    height.value = window.innerHeight - heightOffset
+    emit('resized', { cols: cols.value, size: { width: width.value, height: height.value } })
+  })
+  onUnmounted(() => {})
+  return { cols, width, height }
+}
