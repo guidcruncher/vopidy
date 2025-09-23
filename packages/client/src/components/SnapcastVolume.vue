fb71a29087f832a71792dbd636d408fca50e2c89
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-23 19:54:53 +0000

    UI Tweaks to Mixer desk

--- packages/client/src/components/SnapcastVolume.vue
+++ packages/client/src/components/SnapcastVolume.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="pa-2">
     <h3>Snapcast Client Volumes</h3>
-    <div v-for="item in clients" :key="item.id">
-      <v-slider
-        min="0"
-        max="100"
-        show-ticks
-        tick-size="1"
-        persistent-hint
-        step="1"
-        thumb-label
-        :label="item.config.name"
-        v-model="item.config.volume.percent"
-        @end="setVolume(item.id, item.config.volume.percent)"
-      >
-        <template v-slot:prepend>
-          <v-btn
-            v-if="!item.config.volume.muted"
-            icon="mdi-volume-mute"
-            variant="outlined"
-            density="compact"
-            @click="setMute(item.id, true)"
-          ></v-btn>
-          <v-btn
-            v-if="item.config.volume.muted"
-            icon="mdi-volume-high"
-            variant="outlined"
-            density="compact"
-            @click="setMute(item.id, false)"
-          ></v-btn>
-        </template>
-      </v-slider>
-    </div>
+    <v-sheet v-for="group in groups" class="pa-2 rounded" border="primary">
+      {{ group.name }}
+      <div v-for="item in group.clients" :key="item.id">
+        <v-slider
+          min="0"
+          max="100"
+          show-ticks
+          tick-size="1"
+          persistent-hint
+          step="1"
+          thumb-label
+          :label="item.config.name"
+          v-model="item.config.volume.percent"
+          @end="setVolume(item.id, item.config.volume.percent)"
+        >
+          <template v-slot:prepend>
+            <v-btn
+              v-if="!item.config.volume.muted"
+              icon="mdi-volume-mute"
+              variant="outlined"
+              density="compact"
+              @click="setMute(item.id, true)"
+            ></v-btn>
+            <v-btn
+              v-if="item.config.volume.muted"
+              icon="mdi-volume-high"
+              variant="outlined"
+              density="compact"
+              @click="setMute(item.id, false)"
+            ></v-btn>
+          </template>
+        </v-slider>
+      </div>
+    </v-sheet>
   </div>
 </template>
 <style></style>
@@ -43,12 +46,12 @@ export default {
   name: 'VolumeMixer',
   props: {},
   data() {
-    return { clients: [] }
+    return { groups: [] }
   },
   mounted() {
     vopidy('snapcast.status', []).then((res) => {
       if (res.result.ok) {
-        this.clients = res.result.result.server.groups[0].clients
+        this.groups = res.result.result.server.groups
       }
     })
   },
@@ -58,7 +61,7 @@ export default {
       vopidy('snapcast.setvolume', { id: id, muted: mute, level: undefined }).then((res) => {
         vopidy('snapcast.status', []).then((res) => {
           if (res.result.ok) {
-            this.clients = res.result.result.server.groups[0].clients
+            this.groups = res.result.result.server.groups
           }
         })
       })
@@ -67,7 +70,7 @@ export default {
       vopidy('snapcast.setvolume', { id: id, mute: undefined, level: level }).then((res) => {
         vopidy('snapcast.status', []).then((res) => {
           if (res.result.ok) {
-            this.clients = res.result.result.server.groups[0].clients
+            this.groups = res.result.result.server.groups
           }
         })
       })
