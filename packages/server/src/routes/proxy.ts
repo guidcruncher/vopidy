0412aff277e666cd0e74296e8d3614bc346e1ef7
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-10-16 11:52:24 +0100

    Imprived UI responsiveness

--- packages/server/src/routes/proxy.ts
+++ packages/server/src/routes/proxy.ts
@@ -1,5 +1,4 @@
 import { Config } from "@/core/config"
-import { logger } from "@/core/logger"
 import * as crypto from "crypto"
 import * as fs from "fs"
 import { Hono } from "hono"
@@ -15,51 +14,48 @@ const isImageCacheEnabled = () => {
   return cfg.enableImageCache
 }
 
+const getNotCached = async (c, url) => {
+  const cdata = await fetch(url)
+  if (cdata.ok) {
+    const data = await new sharp(await cdata.bytes())
+      .resize(300, 300, { fit: "contain" })
+      .toFormat("webp")
+      .toBuffer()
+    return c.body(data, { headers: { "Content-Type": "image/webp" } })
+  }
+  return c.notFound()
+}
+
 export const proxyRoute = new Hono()
 
 proxyRoute.get("/", async (c) => {
-  try {
-    let contenttype = "image/webp"
-    const url = Buffer.from(decodeURIComponent(c.req.query("u")), "base64").toString("ascii")
-    let bytes = undefined
-    const hash = crypto.createHash("sha256").update(url).digest("hex")
+  const contenttype = "image/webp"
+  const url = Buffer.from(decodeURIComponent(c.req.query("u")), "base64").toString("ascii")
+  let bytes = undefined
+  const hash = crypto.createHash("sha256").update(url).digest("hex")
 
-    if (isImageCacheEnabled()) {
-      const cacheFile = path.join(process.env.VOPIDY_CACHE.toString() + "/", hash)
-      if (fs.existsSync(cacheFile)) {
-        bytes = fs.readFileSync(cacheFile)
-      } else {
-        const data = await fetch(url)
-        if (data.ok) {
-          const Sharp = new sharp(await data.bytes())
-            .resize(300, 300, { fit: "contain" })
-            .toFormat("webp")
-            .toBuffer()
-            .then((data) => {
-              fs.writeFileSync(cacheFile, data)
-              return c.body(data, { headers: { "Content-Type": "image/webp" } })
-            })
-        } else {
-          return c.notFound()
-        }
-      }
-    } else {
-      const cdata = await fetch(url)
-      if (cdata.ok) {
-        const Sharp = new sharp(await cdata.bytes())
-          .resize(300, 300, { fit: "contain" })
-          .toFormat("webp")
-          .toBuffer()
-          .then((data) => {
-            return c.body(data, { headers: { "Content-Type": "image/webp" } })
-          })
-      } else {
-        return c.notFound()
-      }
-    }
-  } catch (err) {
-    logger.error("Error in proxy", err)
-    c.status(404)
-    return c.notFound()
+  if (!isImageCacheEnabled()) {
+    return getNotCached(c, url)
   }
+
+  const cacheFile = path.join(process.env.VOPIDY_CACHE.toString() + "/", hash)
+
+  if (fs.existsSync(cacheFile)) {
+    bytes = fs.readFileSync(cacheFile)
+  } else {
+    const data = await fetch(url)
+    if (data.ok) {
+      bytes = await new sharp(await data.bytes())
+        .resize(300, 300, { fit: "contain" })
+        .toFormat("webp")
+        .toBuffer()
+
+      fs.writeFileSync(cacheFile, bytes)
+    }
+  }
+  if (bytes) {
+    return c.body(bytes, { headers: { "Content-Type": "image/webp" } })
+  }
+
+  return c.notFound()
 })
